94b763cb67981b38440126fa444ede4e27fdae6f
Shvorobey <a.shvorobey@gmail.com>
2019-05-07 17:15:01 +0300

    Well done дополнительная проверка

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$phrase = '(({{[[}}]])){}}'; // проверяемое выражение
+$phrase = '32323233}(({{[[}}]])){}'; // проверяемое выражение
 $array = str_split ($phrase); // преобразование строки в массив
 $size = count ($array); // вычисление размера массива
 $opening_bracket=0; // инициализация переменных количества различных скобок
@@ -21,10 +21,10 @@ if (strlen($phrase)>0){ // проверка на наличие проверяе
         if($array[$i]=='{') $opening_brace++;
         if($array[$i]=='}') $closing_brace++;
         if ($opening_bracket < $closing_bracket or $opening_square_bracket < $closing_square_bracket or  $opening_brace < $closing_brace)
-            exit("Ошибка! Закрывающаяся скобка идет строго после открывающейся");
+            exit("Ошибка! Закрывающаяся скобка идет строго после открывающейся"); // закрывающая скобка не может идти раньше открывающей
     }
     if ($opening_bracket==$closing_bracket && $opening_square_bracket==$closing_square_bracket && $opening_brace==$closing_brace)
-        echo "Верно"; // проверка соответствия количества различніх скобок
+        echo "Верно"; // проверка соответствия количества различных скобок
     else
         echo "Не верно";
 }
