fbb83c8520b7bdca9f5626ea1778081ad534b43e
Shvorobey <a.shvorobey@gmail.com>
2019-05-08 14:52:21 +0300

    Исправленное

--- index.php
+++ index.php
@@ -1,36 +1,63 @@
 <?php
 
-$phrase = '32323233((({{[[}}]])){}'; // проверяемое выражение
-$array = str_split ($phrase); // преобразование строки в массив
-$size = count ($array); // вычисление размера массива
-$opening_bracket=0; // инициализация переменных количества различных скобок
-$closing_bracket=0;
-$opening_square_bracket=0;
-$closing_square_bracket=0;
-$opening_brace=0;
-$closing_brace=0;
-
-if (strlen($phrase)>0){    // проверка на наличие проверяемой строки
-if ($array[0]==')'or $array[0]==']' or $array[0]=='}' or $array[$size-1]=='(' or $array[$size-1]=='['or $array[$size-1]=='{')
-    exit("Проверте правильность введенной строки"); // проверка первого и последнего символов
-
-    for($i=0; $i<$size; $i++) {
-        if($array[$i]=='(') $opening_bracket++; // подсчет количества различного вида скобок
-        if($array[$i]==')') $closing_bracket++;
-        if($array[$i]=='[') $opening_square_bracket++;
-        if($array[$i]==']') $closing_square_bracket++;
-        if($array[$i]=='{') $opening_brace++;
-        if($array[$i]=='}') $closing_brace++;
-        if ($opening_bracket < $closing_bracket or $opening_square_bracket < $closing_square_bracket or  $opening_brace < $closing_brace)
-            exit("Ошибка! Закрывающаяся скобка идет строго после открывающейся"); // закрывающая скобка не может идти раньше открывающей
+function check($phrase) {
+    $result = 'Верно';
+    $pairs = [
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+    ];
+    $array = [];
+    $phrase = preg_replace('~[^\(\)\{\}\[\]]~', '', $phrase);
+  $size=strlen($phrase);
+    if ($size>0) {
+        for ($i = 0; $i < $size; $i++) {
+            $var = substr($phrase, $i, 1);
+            if (!array_key_exists($var, $pairs) || end($array) !== $pairs[$var]) {
+                array_push($array, $var);
+            } else {
+                array_pop($array);
+            }
+        }
+        if (count($array) > 0) {
+            $res = 'Не верно';
+        }
     }
-    if ($opening_bracket==$closing_bracket && $opening_square_bracket==$closing_square_bracket && $opening_brace==$closing_brace)
-        echo "Верно"; // проверка соответствия количества различных скобок
-    else
-        echo "Не верно";
+    return $result;
 }
-else
-    echo 'Введите строку для проверки';
+
+
+//$phrase = '32323233((({{[[}}]])){}'; // проверяемое выражение
+//$array = str_split ($phrase); // преобразование строки в массив
+//$size = count ($array); // вычисление размера массива
+//$opening_bracket=0; // инициализация переменных количества различных скобок
+//$closing_bracket=0;
+//$opening_square_bracket=0;
+//$closing_square_bracket=0;
+//$opening_brace=0;
+//$closing_brace=0;
+//
+//if (strlen($phrase)>0){    // проверка на наличие проверяемой строки
+//if ($array[0]==')'or $array[0]==']' or $array[0]=='}' or $array[$size-1]=='(' or $array[$size-1]=='['or $array[$size-1]=='{')
+//    exit("Проверте правильность введенной строки"); // проверка первого и последнего символов
+//
+//    for($i=0; $i<$size; $i++) {
+//        if($array[$i]=='(') $opening_bracket++; // подсчет количества различного вида скобок
+//        if($array[$i]==')') $closing_bracket++;
+//        if($array[$i]=='[') $opening_square_bracket++;
+//        if($array[$i]==']') $closing_square_bracket++;
+//        if($array[$i]=='{') $opening_brace++;
+//        if($array[$i]=='}') $closing_brace++;
+//        if ($opening_bracket < $closing_bracket or $opening_square_bracket < $closing_square_bracket or  $opening_brace < $closing_brace)
+//            exit("Ошибка! Закрывающаяся скобка идет строго после открывающейся"); // закрывающая скобка не может идти раньше открывающей
+//    }
+//    if ($opening_bracket==$closing_bracket && $opening_square_bracket==$closing_square_bracket && $opening_brace==$closing_brace)
+//        echo "Верно"; // проверка соответствия количества различных скобок
+//    else
+//        echo "Не верно";
+//}
+//else
+//    echo 'Введите строку для проверки';
 
 
 //   Это ответ на тестовое задание №2:
